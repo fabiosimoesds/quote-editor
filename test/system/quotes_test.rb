0f5d4228e6781b2694859a6e74032e142626de0b
fabiosimoesds <fabio.simoesds@gmail.com>
2022-10-31 22:02:59 +0000

    Making Turbo-rails work without any JS.

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -2,7 +2,7 @@ require "application_system_test_case"
 
 class QuotesTest < ApplicationSystemTestCase
   setup do
-    @quote = quotes(:first) # Reference to the first fixture quote
+    @quote = Quote.ordered.first # Reference to the first fixture quote
   end
   test "Create a new quote" do
     # When we visit the Quotes#index page we expecte to see a title with the text "Quotes"
