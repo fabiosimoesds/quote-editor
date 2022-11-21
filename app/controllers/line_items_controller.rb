ea774b3bb46389f6f73752a5cef1d7a07853fe2c
fabiosimoesds <fabio.simoesds@gmail.com>
2022-11-21 21:47:07 +0000

    All CRUD actions now working in a one pager using Turbo Rails

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,6 @@
 class LineItemsController < ApplicationController
   before_action :set_line_item, only: %i[edit update destroy]
-  before_action :set_line_item_date, only: %i[new create edit]
+  before_action :set_line_item_date, only: %i[new create edit update]
   before_action :set_quote
 
   def new
@@ -11,7 +11,10 @@ class LineItemsController < ApplicationController
     @line_item = @line_item_date.line_items.build(line_item_params)
 
     if @line_item.save
-      redirect_to quote_path(@quote), notice: "Item was successfully created."
+      respond_to do |format|
+        format.html { redirect_to quote_path(@quote), notice: "Item was successfully created." }
+        format.turbo_stream { flash.now[:notice] = "Item was successfully created." }
+      end
     else
       render :new, status: :unprocessable_entity
     end
@@ -22,7 +25,10 @@ class LineItemsController < ApplicationController
 
   def update
     if @line_item.update(line_item_params)
-      redirect_to quote_path(@quote), notice: "Item was successfully updated"
+      respond_to do |format|
+        format.html { redirect_to quote_path(@quote), notice: "Item was successfully updated." }
+        format.turbo_stream { flash.now[:notice] = "Item was successfully updated." }
+      end
     else
       render :edit, status: :unprocessable_entity
     end
@@ -30,7 +36,11 @@ class LineItemsController < ApplicationController
 
   def destroy
     @line_item.destroy
-    redirect_to quote_path(@quote), notice: "Item was successfully deleted."
+
+    respond_to do |format|
+      format.html { redirect_to quote_path(@quote), notice: "Date was successfully destroyed." }
+      format.turbo_stream { flash.now[:notice] = "Date was successfully destroyed." }
+    end
   end
 
   private
