f5a85529084b5ae687f31bfab1fe0cefbf07ca4d
fabiosimoesds <fabio.simoesds@gmail.com>
2022-11-21 19:08:16 +0000

    Add line_item model, run migration, added seeding, added validation and model relationships

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :quotes do
-    resources :line_item_dates, except: %i[index show]
+    resources :line_item_dates, except: %i[index show] do
+      resources :line_items, except: %i[index show]
+    end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
